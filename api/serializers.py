ad1f63f1a50056b0ac855015d0bc93e92cf6dc16
bennet <34bennet@gmail.com>
2022-11-14 13:04:11 +0530

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from api.models import Products
+
+class ProductSerializer(serializers.ModelSerializer):
+    id=serializers.CharField(read_only=True)
+
+    class Meta:
+        model=Products
+        fields="__all__"
+
---
+++ api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import permissions,authentication
+
+from api.serializers import ProductSerializer
+from api.models import Products
+
+class Products
+
+
+# Create your views here.
